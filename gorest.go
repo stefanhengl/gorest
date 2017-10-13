0fe568b45dcf1dd4a3dae5da4091e129d926bdbf
D055847 <stefan.hengl@sap.com>
2017-10-13 12:33:40 +0200

    buf-fix: add ampersand between query parameters

--- gorest.go
+++ gorest.go
@@ -3,6 +3,7 @@ package gorest
 import (
 	"bytes"
 	"fmt"
+	"strings"
 )
 
 /*
@@ -13,7 +14,25 @@ func MapToQueryString(m map[string]string) string {
 	var buffer bytes.Buffer
 	buffer.WriteString("?")
 	for k, v := range m {
-		buffer.WriteString(fmt.Sprintf("%s=%s", k, v))
+		buffer.WriteString(fmt.Sprintf("%s=%s&", k, v))
 	}
-	return buffer.String()
+	bufferString := buffer.String()
+	return bufferString[:len(bufferString)-1]
+}
+
+/*
+Join joins parts of a URL including the query string
+*/
+func Join(parts []string) string {
+	url := strings.Trim(parts[0], "/")
+	parts = parts[1:]
+	for _, part := range parts {
+		cleanPart := strings.Trim(part, "/")
+		if strings.HasPrefix(cleanPart, "?") {
+			url = strings.Join([]string{url, cleanPart}, "")
+			continue
+		}
+		url = strings.Join([]string{url, cleanPart}, "/")
+	}
+	return url
 }
